eaddba7331c1e60aefa976d714886f97cbd21f7b
roodrigo <francisco@miflink.com>
2022-01-11 15:11:50 -0600

    Feat: Added  retwet

--- twit/admin.py
+++ twit/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from twit.models import Twit, Comment, Like
+from twit.models import Twit, Comment, Like, ReTwit
 # Register your models here.
 
 admin.site.register(Twit)
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(ReTwit)
+
