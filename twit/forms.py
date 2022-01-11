334950f9fc71dff050995e067feba435abc81143
roodrigo <francisco@miflink.com>
2022-01-10 22:34:53 -0600

    Feat: Added comments and like to templates and views

--- twit/forms.py
+++ twit/forms.py
@@ -2,4 +2,11 @@ from django import forms
 
 
 class TwitForm(forms.Form):
-    twit = forms.CharField(max_length=60)
+    twit = forms.CharField(max_length=60, required=True)
+
+
+class CommentForm(forms.Form):
+
+    comment = forms.CharField(max_length=150, required=True)
+
+
--- twit/urls.py
+++ twit/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from twit.views import CreateTwit
+from twit.views import CreateTwit, CommentCreateView, LikeCreateView
 
 app_name = "twit"
 
 urlpatterns = [
     path('add/', CreateTwit.as_view(), name="create-twit"),
+    path('<int:pk>/comment/add/', CommentCreateView.as_view(), name="comment"),
+    path('<int:pk>/like/add/', LikeCreateView.as_view(), name="like"),
 ]
 
 
